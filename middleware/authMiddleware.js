2712c3f4ccd64de36b834c2af9c4e96ab68851c8
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-05-05 23:05:57 +0530

    Add: Admin auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 // Model
 const userModel = require("../models/userModel");
 const vendorModel = require("../models/vendorModel");
+const adminModel = require("../models/adminModel");
 
 // ------------------------------------------------------------------------------------------------------------
 
@@ -38,6 +39,27 @@ exports.isVendor = asyncHandler(async (req, res, next) => {
   try {
     const decoded = await jwt.verify(access_token, process.env.SECRET_KEY);
     req.user = await vendorModel.findById(decoded.id);
+    if (!req.user) {
+      res.status(401);
+      throw new Error("Not Authorized");
+    }
+    next();
+  } catch (error) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+});
+
+// Admin Auth
+exports.isAdmin = asyncHandler(async (req, res, next) => {
+  const { access_token } = req.cookies;
+  if (!access_token) {
+    res.status(401);
+    throw new Error("No Token, Not Authorized");
+  }
+  try {
+    const decoded = await jwt.verify(access_token, process.env.SECRET_KEY);
+    req.user = await adminModel.findById(decoded.id);
     if (!req.user) {
       res.status(401);
       throw new Error("Not Authorized");
